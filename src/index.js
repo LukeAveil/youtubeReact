9c9f871f22df5cfdc7451bee2b510eb69e9e5f8b
Luke <lukeaveil@yahoo.com>
2016-10-31 11:56:30 +0000

    creates searchbar components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyBDGvKrxXqvXMm_8IdF8Non6Hh80k1UUjo';
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector('.container'));
